6056a3a2aafcbb70f111e5a51f98d315571f05b0
Walt Boling <bolingjw@gmail.com>
2018-03-12 00:27:58 -0400

    This version completes the exercise, adding: multiple sections, insert/delete/move rows functionality. The problem persists, however, regarding showing the images if they are within the "images" folder. Once removed and placed into the project, everything works as it should.

--- TableViewDemo/TableViewController.swift
+++ TableViewDemo/TableViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class TableViewController: UIViewController {
     
+    var otherItems = [DataItem]()
+    var allItems = [[DataItem]]()
     var items = [DataItem]()
     
     @IBOutlet weak var tableView: UITableView!
@@ -24,35 +26,146 @@ class TableViewController: UIViewController {
                 items.append(DataItem(title: "Title #0\(i)", subtitle: "This is subtitle #0\(i)", imageName: "img0\(i).jpg"))
             }
         }
+        for i in 1...12 {
+            if i > 9 {
+                otherItems.append(DataItem(title: "Another Title #\(i)", subtitle: "This is another subtitle #\(i)", imageName: "anim\(i).jpg"))
+            } else {
+                otherItems.append(DataItem(title: "Another Title #0\(i)", subtitle: "This is another subtitle #0\(i)", imageName: "anim0\(i).jpg"))
+            }
+        }
+        
+        navigationItem.rightBarButtonItem = editButtonItem
+        
+        
+    allItems.append(items)
+    allItems.append(otherItems)
+    
+    tableView.allowsSelectionDuringEditing = true
+    }
+    
+    override func setEditing(_ editing: Bool, animated: Bool) {
+        super.setEditing(editing, animated: animated)
+      
+        if editing {
+            tableView.beginUpdates()
+            
+            for (index, sectionItems) in allItems.enumerated() {
+                let indexPath = IndexPath (row: sectionItems.count, section: index)
+                tableView.insertRows(at: [indexPath], with: .fade)
+            }
+            
+            tableView.endUpdates()
+            tableView.setEditing(true, animated: true)
+        } else {
+            tableView.beginUpdates()
+            
+            for (index, sectionItems) in allItems.enumerated() {
+                let indexPath = IndexPath(row: sectionItems.count, section: index)
+                tableView.deleteRows(at: [indexPath], with: .fade)
+            }
+            
+            tableView.endUpdates()
+            tableView.setEditing(false, animated: true)
+        }
     }
    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return items.count
+        let addedRow = isEditing ? 1 : 0
+        
+        return allItems[section].count + addedRow
     }
     
     func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
+        return allItems.count
     }
+ 
 }
 
 extension TableViewController: UITableViewDataSource {
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-        let item = items[indexPath.row]
         
-        cell.textLabel?.text = item.title
-        cell.detailTextLabel?.text = item.subtitle
-        
-        if let imageView = cell.imageView, let itemImage = item.image {
-            imageView.image = itemImage
-        } else {
+        if indexPath.row >= allItems[indexPath.section].count && isEditing {
+            cell.textLabel?.text = "Add New Item"
+            cell.detailTextLabel?.text = nil
             cell.imageView?.image = nil
+        } else {
+            let item = allItems[indexPath.section] [indexPath.row]
+            
+            cell.textLabel?.text = item.title
+            cell.detailTextLabel?.text = item.subtitle
+            
+            if let imageView = cell.imageView, let itemImage = item.image {
+                imageView.image = itemImage
+            } else {
+                cell.imageView?.image = nil
+            }
         }
+        
         return cell
     }
-}
     
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            allItems[indexPath.section].remove(at:indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            let newData = DataItem(title: "New Data", subtitle: "", imageName: nil)
+            allItems[indexPath.section].append(newData)
+            tableView.insertRows(at: [indexPath], with: .fade)
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return "Section #\(section)"
+    }
+    
+    func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
+        let itemToMove = allItems[sourceIndexPath.section] [sourceIndexPath.row]
+        
+        allItems[sourceIndexPath.section].remove(at: sourceIndexPath.row)
+        
+        if sourceIndexPath.section == destinationIndexPath.section {
+            allItems[sourceIndexPath.section].insert(itemToMove, at: destinationIndexPath.row)
+        }
+    }
+}
+
+
 extension TableViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCellEditingStyle {
+        let sectionItems = allItems[indexPath.section]
+        if indexPath.row >= sectionItems.count {
+            return .insert
+        } else {
+            return .delete
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, willSelectRowAt indexPath: IndexPath) -> IndexPath? {
+        let sectionItems = allItems[indexPath.section]
+        if isEditing && indexPath.row < sectionItems.count {
+            return nil
+        }
+        return indexPath
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        let sectionItems = allItems[indexPath.section]
+        if indexPath.row >= sectionItems.count && isEditing {
+            self.tableView(tableView, commit: .insert, forRowAt: indexPath)
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, targetIndexPathForMoveFromRowAt sourceIndexPath: IndexPath, toProposedIndexPath proposedDestinationIndexPath: IndexPath) -> IndexPath {
+        let sectionItems = allItems[proposedDestinationIndexPath.section]
+        if proposedDestinationIndexPath.row >= sectionItems.count {
+            return IndexPath(row: sectionItems.count - 1, section: proposedDestinationIndexPath.section)
+        }
+        return proposedDestinationIndexPath
+    }
+    
 }
 
